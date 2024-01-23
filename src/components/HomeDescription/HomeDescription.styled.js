4efd791c47712e964bb4a35bf4a4573ec6638180
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-01-23 19:56:30 +0200

    fix styles

--- src/components/HomeDescription/HomeDescription.styled.js
+++ src/components/HomeDescription/HomeDescription.styled.js
@@ -1,32 +1,49 @@
 import styled from "styled-components";
-import HomeBG from '../../img/Home-bg.webp';
+import HomeBG from "../../img/Home-bg.webp";
 
 export const StyledDivWrapper = styled.div`
-    width: 100%;
-    height: 500px;
-    background-image: url(${HomeBG});
-    background-size: cover;
-    background-repeat: non-repeat;
-    background-position: center;
-    padding-top: 140px;
+  width: 100%;
+  height: 500px;
+  background-image: url(${HomeBG});
+  background-size: cover;
+  background-repeat: non-repeat;
+  background-position: center;
+  padding-top: 140px;
 
-    @media only screen and (max-width: 1430px) {
-        padding-top: 40px;
-      }
-`
+  @media only screen and (max-width: 1430px) {
+    padding-top: 40px;
+  }
+  @media only screen and (max-width: 767px) {
+    height: auto;
+    padding-top: 150px;
+  }
+`;
 
 export const StyledArticle = styled.article`
-width: 45%;
-    color: var(--description-text);
+  width: 45%;
+  color: var(--description-text);
 
-    & h1 {
-        font-family: "Manrope-semibold";
-        font-size: 35px;
-        margin-bottom: 20px;
-    }
+  @media only screen and (max-width: 767px) {
+    width: 100%;
+  }
 
+  & h1 {
+    font-family: "Manrope-semibold";
+    font-size: 35px;
+    margin-bottom: 20px;
+    @media only screen and (max-width: 767px) {
+    padding-bottom: 50px;
+      }
+  }
+
+  & p {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+
+  @media only screen and (max-width: 767px) {
     & p {
-        font-size: 24px;
-        margin-bottom: 20px;
+      display: none;
     }
-`
+  }
+`;
