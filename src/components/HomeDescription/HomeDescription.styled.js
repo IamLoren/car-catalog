674d56014afea7fa7dd046f9ea9e0da82aeefa9c
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-01-25 00:08:17 +0200

    fix dark photo

--- src/components/HomeDescription/HomeDescription.styled.js
+++ src/components/HomeDescription/HomeDescription.styled.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import HomeBG from "../../img/Home-bg.webp";
+// import HomeBG from "../../img/Home-bg.webp";
 
 export const StyledDivWrapper = styled.div`
   width: 100%;
   height: 500px;
-  background-image: url(${HomeBG});
+  background-image: var(--home-bg);
   background-size: cover;
   background-repeat: non-repeat;
   background-position: center;
