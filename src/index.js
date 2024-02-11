ba0655dba3bab54ab21ce4a4cdfb9b70fcf2bf01
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-02-11 17:02:08 +0200

    fix path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store.js';
 import Global from './css/common.js';
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/car-catalog">
+  <HashRouter>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
         <App />
@@ -19,6 +19,6 @@ root.render(
     </PersistGate>
     
 
-  </BrowserRouter>
+  </HashRouter>
 );
 
